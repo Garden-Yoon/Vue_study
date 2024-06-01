179d0da72d26196297fa474469fbc768bcd6b9e4
jwyoon <jungwon.yoon94@gmail.com>
2024-06-01 15:14:51 +0900

    vue2강 데이터바인딩

--- vuedongsan/src/App.vue
+++ vuedongsan/src/App.vue
@@ -1,17 +1,39 @@
-<!-- 테스트완료입니다. 굿입니다~~ 변경하겠습니다. 라스트 변경입니다 -->
+<!-- template 안에는 html을 짜고
+script 안에는 JS 짜고
+style 안에는 CSS를 짠다 -->
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <h4>ㅇㅇ 원룸</h4>
+    <!-- {{ 콧수염 안에 데이터 이름을 기입}}
+           밑에 있는 데이터가 연결됨  -->
+    <p>{{ price1 }} 만원</p>
+  </div>
+  <div>
+    <h4>ㅁㅁ 원룸</h4>
+    <p>0000 만원</p>
+  </div>
+  <div>
+    <h4>ㅂㅂ 원룸</h4>
+    <p>0000 만원</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+// 데이터 바인딩 : JS데이터를 HTML에 꽂아넣는 문법
+// 기존 JS : document.getElementById().innerHTML = ??
+// Vue : 데이터를 data() {return _{여기에 데이터 보관}}안에 넣는다
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      // 데이터는 object 자료로 저장한다 (작명 : 저장할 값)
+      price1: 60,
+      price2: 70,
+      price3: 80,
+    };
   },
+  components: {},
 };
 </script>
 
